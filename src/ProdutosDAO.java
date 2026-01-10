892ad90719556dffcfec78ea58e641ca8c263435
Fellipe Lopes <fellipe561@gmail.com>
2026-01-10 14:53:43 -0300

    funcionalidade para salvar os produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
 public class ProdutosDAO {
@@ -22,12 +23,24 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    public void cadastrarProduto (ProdutosDTO produto) throws SQLException{
+        conn = new conectaDAO().connectDB();
+
+        String sql = "INSERT INTO produtos(nome, valor, status) VALUES(?,?,?)";
+
+        try{
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, "A Venda");
+
+            prep.executeUpdate();
+           
+        }catch(SQLException e){
+            System.out.println(e);
+        }finally{
+            conn.close();
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
